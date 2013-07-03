438b603b9d5b04013e5af521f441a47f6563f81b
Rob Story <wrobstory@gmail.com>
2013-07-02 22:42:38 -0700

    Refactoring into Constructor, added simple Markers with popups

--- dist/twig.js
+++ dist/twig.js
@@ -1,35 +1,56 @@
 /*! twig - v0.0.1
  *  License: MIT
- *  Date: 2013-07-01
+ *  Date: 2013-07-02
  */
 (function( global ) {
-  var twig = (function() {
+  var Twig = function() {
 
     var L = window.L;
 
-    baseMap = function(lat, lng, zoomstart) {
-        return L.map('map').setView([lat, lng], zoomstart);
+    baseMap = function(lat, lng, zoomstart, id) {
+        return L.map(id).setView([lat, lng], zoomstart);
     };
 
     tileLayer = function(map, tileref, maxzoom, attribution) {
         L.tileLayer(tileref, { maxZoom: maxzoom, attribution: attribution}).addTo(map);
     };
 
-    parse = function(data) {
+    markers = function(map, markers) {
+        leafmarks = markers.map(function(marker) {
+          var leafmark = L.marker([marker['lat'], marker['lng']]);
+          if ('popup' in marker) {
+            leafmark.bindPopup(marker['popup']['data']);
+          }
+          return leafmark;
+        });
 
-        var map = baseMap(data['lat'], data['lng'], data['zoomstart']);
-        tileLayer(map, data['tileref'], data['maxZoom'], data['attribution']);
+        leafmarks.map(function(mark){map.addLayer(mark);});
 
     };
 
-    return {
-      sprout: function(data) {
-        parse(data);
-      }
-     };
+    parse = function(data, id) {
 
-  })();
+        var map = baseMap(data['lat'], data['lng'], data['zoomstart'], id);
+        tileLayer(map, data['tileref'], data['maxZoom'], data['attribution']);
 
-  global.twig = twig;
+        if ('markers' in data) {
+          markers(map, data['markers']);
+        }
+
+    };
+
+    this.sprout = function(data, id) {
+        parse(data, id);
+      };
+
+    return this;
+
+  };
+
+  var twig = function() {
+    return new Twig();
+  };
+
+  global.twig = twig();
 
 })( this );
